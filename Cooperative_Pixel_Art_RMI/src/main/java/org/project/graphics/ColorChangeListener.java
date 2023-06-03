3b4e3f3ac340e6bf348ec545bbe43f3d626153c8
davideboldini <118013797+davideboldini@users.noreply.github.com>
2023-06-03 10:55:10 +0200

    first commit

---
+++ Cooperative_Pixel_Art_RMI/src/main/java/org/project/graphics/ColorChangeListener.java
@@ -0,0 +1,5 @@
+package org.project.graphics;
+
+public interface ColorChangeListener {
+    void colorChanged(int color);
+}
---
+++ Cooperative_Pixel_Art_RMI/src/main/java/org/project/graphics/PixelGridEventListener.java
@@ -0,0 +1,5 @@
+package org.project.graphics;
+
+public interface PixelGridEventListener {
+	void selectedCell(int x, int y);
+}
