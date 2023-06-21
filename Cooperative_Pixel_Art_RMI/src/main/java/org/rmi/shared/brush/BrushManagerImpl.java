85b9c7c8f564e18c4f68ff2030bb3bdecf11843b
Davide Boldini <davide.boldini@studio.unibo.it>
2023-06-21 19:05:17 +0200

    removed synchronized keyword

--- Cooperative_Pixel_Art_RMI/src/main/java/org/rmi/shared/brush/BrushManagerImpl.java
+++ Cooperative_Pixel_Art_RMI/src/main/java/org/rmi/shared/brush/BrushManagerImpl.java
@@ -16,26 +16,26 @@ public class BrushManagerImpl implements BrushManager, Serializable {
     private final Log logger = new LogImpl();
     private final Map<String, Brush> brushMap = new HashMap<>();
 
-    public synchronized void addBrush(final MessageConnection message) {
+    public void addBrush(final MessageConnection message) {
         logger.joinedUserMex(message.getIdSender());
         brushMap.put(message.getIdSender(), message.getBrush());
     }
 
-    public synchronized void removeBrush(final MessageClose message) {
+    public void removeBrush(final MessageClose message) {
         logger.quitUserMex(message.getIdSender());
         brushMap.remove(message.getIdSender());
     }
 
-    public synchronized void updateBrushPosition(final MessagePosition message) {
+    public void updateBrushPosition(final MessagePosition message) {
         brushMap.get(message.getIdSender()).updatePosition(message.getPosition().getX(), message.getPosition().getY());
     }
 
-    public synchronized void updateBrushColor(final MessageColorChanged message) {
+    public void updateBrushColor(final MessageColorChanged message) {
         logger.changeColorUserMex(message.getIdSender(), message.getColor());
         brushMap.get(message.getIdSender()).setColor(message.getColor());
     }
 
-    public synchronized Map<String, Brush> getBrushMap() {
+    public Map<String, Brush> getBrushMap() {
         return brushMap;
     }
 }
